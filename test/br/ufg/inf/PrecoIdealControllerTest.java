994e1681bcb1991b3a4c793ee29f5ccd6d7a575f
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-11 17:44:15 -0300

    Remove imports inutilizados

--- test/br/ufg/inf/PrecoIdealControllerTest.java
+++ test/br/ufg/inf/PrecoIdealControllerTest.java
@@ -1,9 +1,5 @@
 package br.ufg.inf;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
