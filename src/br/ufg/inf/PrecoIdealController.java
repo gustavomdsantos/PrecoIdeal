a55dce1cd011a33ba2230d27f5812342c27b7077
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-11 18:50:12 -0300

    Implementa função que calcula razão ideal

--- src/br/ufg/inf/PrecoIdealController.java
+++ src/br/ufg/inf/PrecoIdealController.java
@@ -19,6 +19,21 @@ public class PrecoIdealController {
         return Math.abs(elasticidade*100);
     }
 
+    /**
+     * 
+     * Calcula a relação entre o preço final e a quantidade final.
+     * 
+     * @param elasticidade elasticidade do preço na procura
+     * @param pa Preço anterior (preço praticado) (Pa)
+     * @param qa Quantidade atual (normalmente vende....) (Qa)
+     * @return a relação entre o preço final e a quantidade final.
+     */
+    public static double calculaRazaoIdeal(double elasticidade, double pa, double qa){
+
+        double razao = (qa * (elasticidade + 1)) / pa;
+        return(razao);
+    }
+
     public static double calculaVariacaoPreco(double pa, double pf){
 
         double variacao = ((pf-pa)/pa);
