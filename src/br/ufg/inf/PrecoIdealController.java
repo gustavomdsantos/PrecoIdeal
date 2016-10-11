9f8fef0171e7afcf14cf28ae7856815338325c10
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-11 16:24:16 -0300

    Implementa caso base do cálculo da elasticidade do preço na procura

---
+++ src/br/ufg/inf/PrecoIdealController.java
@@ -0,0 +1,21 @@
+package br.ufg.inf;
+
+public class PrecoIdealController {
+
+    /**
+     * Calcula a elasticidade do preço na procura.
+     * @author gustavosotnas
+     * 
+     * @param pa Preço anterior (preço praticado) (Pa)
+     * @param pf Preço final (preço proposto) (Pf)
+     * @param qa Quantidade atual (normalmente vende....) (Qa)
+     * @param qf Quantidade proposta (Qp)
+     * 
+     * @return o épsilon da elasticidade.
+     */
+    public static double calculaElasticidade(double pa, double pf, double qa, double qf) {
+
+        double elasticidade = (((qa-qf)/qf)/(pa-pf)/pf);
+        return elasticidade*(-100);
+    }
+}
---
+++ test/br/ufg/inf/PrecoIdealControllerTest.java
@@ -0,0 +1,28 @@
+package br.ufg.inf;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PrecoIdealControllerTest {
+
+    /**
+     * Caso de exemplo: ingressos de cinema
+     * 
+     * http://www2.anhembi.br/html/ead01/economia/lu04/lo2/index.htm
+     */
+    @Test
+    public void testCalculaElasticidadeCinema() {
+        double pa = 8;
+        double pf = 10;
+        double qa = 4;
+        double qf = 3;
+        double result = PrecoIdealController.calculaElasticidade(pa, pf, qa, qf);
+        double expResult = 1.6;
+        assertEquals(expResult, result, 0.1);
+    }
+    
+}
