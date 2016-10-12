4e3d3bfd22e67317699e5aac11e97260c4f9a353
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-11 21:16:17 -0300

    Formata código fonte

--- test/br/ufg/inf/PrecoIdealControllerTest.java
+++ test/br/ufg/inf/PrecoIdealControllerTest.java
@@ -7,7 +7,7 @@ public class PrecoIdealControllerTest {
 
     /**
      * Caso de exemplo: ingressos de cinema
-     * 
+     *
      * http://www2.anhembi.br/html/ead01/economia/lu04/lo2/index.htm
      */
     @Test
@@ -22,9 +22,8 @@ public class PrecoIdealControllerTest {
     }
 
     /**
-     * Cálculo de variação de preço
-     * Caso de exemplo: calças jeans
-     * 
+     * Cálculo de variação de preço, caso de exemplo: calças jeans
+     *
      * http://pt.wikihow.com/Calcular-a-Varia%C3%A7%C3%A3o-Percentual
      */
     @Test
@@ -43,35 +42,35 @@ public class PrecoIdealControllerTest {
         double pf = 10;
         double qa = 4;
         double qf = 3;
-        
+
         double elasticidade = PrecoIdealController.calculaElasticidade(pa, pf, qa, qf);
-        
-        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa , pf, qa);
+
+        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa, pf, qa);
         assertEquals(qf, result, 0.1);
     }
-    
+
     @Test
     public void testCalculaQtdeIdealFixoP50() {
         double pa = 4;
         double pf = 8;
         double qa = 4;
-        
+
         double elasticidade = -1;
-        
-        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa , pf, qa);
+
+        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa, pf, qa);
         System.out.println("P50: " + result);
         //assertEquals(qf, result, 0.1);
     }
-    
+
     @Test
     public void testCalculaQtdeIdealFixoM50() {
         double pa = 4;
         double pf = 8;
         double qa = 4;
-        
+
         double elasticidade = -0.5;
-        
-        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa , pf, qa);
+
+        double result = PrecoIdealController.calculaQtdeIdealMensal(elasticidade, pa, pf, qa);
         System.out.println("M50: " + result);
         //assertEquals(qf, result, 0.1);
     }
